e637d32449aba0349076444f6291dc96cfeabdfd
Sanjero20 <gelosantos72@gmail.com>
2023-05-24 02:17:25 +0800

    Initial commit

---
+++ pages/_app.tsx
@@ -0,0 +1,15 @@
+import '@/styles/globals.css';
+import type { AppProps } from 'next/app';
+import Head from 'next/head';
+
+export default function App({ Component, pageProps }: AppProps) {
+  return (
+    <>
+      <Head>
+        <title>BSU Alumni</title>
+        <link rel="shortcut icon" href="favicon.ico" type="image/x-icon" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
+}
