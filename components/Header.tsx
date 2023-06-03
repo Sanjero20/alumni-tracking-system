830c929911b4ae79ab2d680812bf8eca5ea30e43
Sanjero20 <gelosantos72@gmail.com>
2023-06-03 18:23:55 +0800

    Minor styles adjustment and additions

--- components/Header.tsx
+++ components/Header.tsx
@@ -8,7 +8,13 @@ function Header({ title = 'Batangas State University' }: Props) {
   return (
     <header className="h-20 bg-primary text-white">
       <div className="container m-auto flex h-full items-center gap-2 p-2 lg:p-0">
-        <Image src={'/bsu-logo.svg'} width={60} height={60} alt="bsu-logo" />
+        <Image
+          src={'/bsu-logo.svg'}
+          width={64}
+          height={64}
+          alt="bsu-logo"
+          className="h-16 w-16"
+        />
         <h1 className="text-lg font-bold lg:text-xl">{title}</h1>
       </div>
     </header>
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,20 @@
-import AuthLayout from '@/layouts/AuthLayout';
+import AuthLayout from '@/services/auth/authLayout';
 import { auth } from '@/firebase/config';
+import Header from '@/components/Header';
+import { useContext } from 'react';
+import { AuthContext } from '@/services/auth/authContext';
+import { useAuthState } from 'react-firebase-hooks/auth';
 
 export default function Home() {
+  const value = useContext(AuthContext);
+
   return (
     <AuthLayout>
-      <p>Landing page</p>
+      <Header />
 
-      <button onClick={() => auth.signOut()}>Signout</button>
+      <button onClick={() => auth.signOut()} className="bg-primary">
+        Signout
+      </button>
     </AuthLayout>
   );
 }
