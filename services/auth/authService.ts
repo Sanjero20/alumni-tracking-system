e27b1a4431171ca5b6f3441d113ed662cb4a8a42
Sanjero20 <gelosantos72@gmail.com>
2023-06-03 18:24:07 +0800

    Add signup functionality

--- services/auth/authService.ts
+++ services/auth/authService.ts
@@ -1,20 +1,54 @@
 import { auth, db } from '@/firebase/config';
-import { signInWithEmailAndPassword } from 'firebase/auth';
+import { collection, doc, setDoc } from 'firebase/firestore';
+import {
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+} from 'firebase/auth';
+
 import { loginErrorHandler } from './authErrorHandler';
+import { AccountRegister } from '@/utils/types';
+
+export const logoutUser = () => {
+  auth.signOut();
+};
 
 export const loginUser = async (email: string, password: string) => {
   try {
     await signInWithEmailAndPassword(auth, email, password);
-    return '';
+    return ''; // No errors
   } catch (error: any) {
     return loginErrorHandler(error.code);
   }
 };
 
-// export const signUpUser =async (registrationData: ) => {
+export const signUpUser = async (registrationData: AccountRegister) => {
+  // Destruct Registration form data
+  const { name, birthday } = registrationData.personalData;
+  const { email, password } = registrationData.accountData;
 
-// }
+  const usersCollection = collection(db, 'users');
 
-export const logoutUser = () => {
-  auth.signOut();
+  try {
+    const userCredential = await createUserWithEmailAndPassword(
+      auth,
+      email,
+      password
+    );
+
+    const { uid } = userCredential.user;
+
+    const docRef = doc(usersCollection, uid);
+    await setDoc(docRef, {
+      id: uid,
+      email,
+      name,
+      birthday,
+    });
+
+    logoutUser();
+    // Reset all forms state
+    return '';
+  } catch (error: any) {
+    return error.code;
+  }
 };
