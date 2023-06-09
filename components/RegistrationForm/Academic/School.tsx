2e67a475e64a5f7940df8b48ac246878fd4f6ca7
Sanjero20 <gelosantos72@gmail.com>
2023-06-09 22:43:07 +0800

    Retrieve and display campuses on option tags

--- components/RegistrationForm/Academic/School.tsx
+++ components/RegistrationForm/Academic/School.tsx
@@ -1,21 +1,44 @@
+import { useEffect, useState } from 'react';
+import { fetchCampuses } from '@/services/request';
+
 function School() {
+  const [campuses, setCampuses] = useState<any>([]);
+
+  useEffect(() => {
+    const getCampuses = async () => {
+      const data = await fetchCampuses();
+      setCampuses(data);
+    };
+
+    getCampuses();
+  }, []);
+
   return (
     <fieldset>
       <div className="flex flex-col">
         <label htmlFor="">Campus</label>
-        <select name="" id="" required>
+        <select name="" required>
           <option value="">Select Campus</option>
+          {campuses.map((campus: any) => (
+            <option key={campus.campusId} value={campus.name}>
+              {campus.name}
+            </option>
+          ))}
         </select>
       </div>
 
-      <div>
+      <div className="flex flex-col">
         <label htmlFor="">College</label>
-        <input type="text" required />
+        <select name="">
+          <option value="">Select College</option>
+        </select>
       </div>
 
-      <div>
+      <div className="flex flex-col">
         <label htmlFor="">Degree/Certification & Major</label>
-        <input type="text" required />
+        <select name="">
+          <option value="">Select Program </option>
+        </select>
       </div>
     </fieldset>
   );
---
+++ services/request.ts
@@ -0,0 +1,30 @@
+import { db } from '@/firebase/config';
+import { collection, getDocs, getDoc, doc } from 'firebase/firestore';
+
+// fetch documents in the database collections
+export const fetchCollection = async (collectionName: string) => {
+  try {
+    const colRef = collection(db, collectionName);
+    const documents = await getDocs(colRef);
+
+    const documentArray: any = [];
+    documents.docs.forEach((document) => {
+      documentArray.push(document.data());
+    });
+
+    return documentArray;
+  } catch (error) {}
+};
+
+export const fetchCampuses = async () => {
+  try {
+    let campuses = await fetchCollection('campuses');
+
+    campuses = campuses.map((campus: any) => ({
+      campusId: campus.campusId,
+      name: campus.name,
+    }));
+
+    return campuses;
+  } catch (error) {}
+};
