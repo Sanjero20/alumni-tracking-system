5c43c3ba9d76e550359aaba12c774d8207671ca6
Sanjero20 <gelosantos72@gmail.com>
2023-06-12 13:59:39 +0800

    Add return type for fetchCollection function

--- services/request.ts
+++ services/request.ts
@@ -1,30 +1,29 @@
 import { db } from '@/firebase.config';
+import { Campus } from '@/types/campus';
 import { collection, getDocs, getDoc, doc } from 'firebase/firestore';
 
 // fetch documents in the database collections
-export const fetchCollection = async (collectionName: string) => {
+export const fetchCollection = async <T> (collectionName: string): Promise<T[]> => {
   try {
     const colRef = collection(db, collectionName);
     const documents = await getDocs(colRef);
 
-    const documentArray: any = [];
+    const documentArray: T[] = [];
     documents.docs.forEach((document) => {
-      documentArray.push(document.data());
+      documentArray.push(document.data() as T);
     });
 
     return documentArray;
-  } catch (error) {}
+  } catch (error) {
+    throw error
+  }
 };
 
 export const fetchCampuses = async () => {
   try {
-    let campuses = await fetchCollection('campuses');
-
-    campuses = campuses.map((campus: any) => ({
-      campusId: campus.campusId,
-      name: campus.name,
-    }));
-
-    return campuses;
-  } catch (error) {}
+    const campusesInfo = await fetchCollection<Campus>('campuses');
+    return campusesInfo;
+  } catch (error) {
+    console.log(error)
+  }
 };
